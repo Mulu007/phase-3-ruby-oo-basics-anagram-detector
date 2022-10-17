957c94099422d7bf9d762eaa4ad51ccd2e2c6d25
Mulu <brianmulu007@gmail.com>
2022-10-17 22:43:44 +0300

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize (word)
+        @word = word
+    end
+
+    def match(string)
+        string.filter do |letters|
+            # splits the letters to an individual letter
+            # sorts the splitted letters alphabetically
+            letters.split("").sort == word.downcase.split("").sort
+        end    
+    end
+end
